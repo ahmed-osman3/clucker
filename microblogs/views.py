38c8cd6dcd12f35a1f1fd74cafb5905f9a2bc827
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-10 12:18:35 +0100

    Create placeholder home page

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request): #request is object created by django containing all infromation recieved by http request.
+   return render(request,'home.html')
