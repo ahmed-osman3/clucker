ddd19f39ff372c3afe0758cfd8d3b00416b6dde8
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-27 12:16:59 +0100

    Add LogInForm with tests

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 from microblogs.forms import SignUpForm
-from microblogs.models import User
+
 
 
 def home(request): #request is object created by django containing all infromation recieved by http request.
@@ -14,15 +14,12 @@ def sign_up(request):
    if request.method == 'POST':
       form = SignUpForm(request.POST)
       if form.is_valid():
-         User.objects.create_user(
-            form.cleaned_data.get('username'),
-            first_name = form.cleaned_data.get('first_name'),
-            last_name = form.cleaned_data.get('last_name'),
-            email = form.cleaned_data.get('email'),
-            bio = form.cleaned_data.get('bio'),
-            password = form.cleaned_data.get('password'),
-         )
+         form.save()
          return redirect('feed')
    else:
       form = SignUpForm()
    return render(request,'sign_up.html',{'form': form})
+
+
+def log_in(request):
+   return render(request,'log_in.html')
